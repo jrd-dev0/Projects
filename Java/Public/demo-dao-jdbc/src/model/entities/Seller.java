bce979f1ef4ec3f4be6b4f766cbd1b4d839424a7
JrD <198513937+jrd-dev0@users.noreply.github.com>
2025-02-12 19:45:14 -0300

    Fixed the declaration of some attributes in the Seller class.
    Corrigido a declaração de alguns atributos na classe Seller.

--- Java/Public/demo-dao-jdbc/src/model/entities/Seller.java
+++ Java/Public/demo-dao-jdbc/src/model/entities/Seller.java
@@ -8,17 +8,17 @@ public class Seller implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private int id;
+	private Integer id;
 	private String name;
 	private String email;
 	private LocalDate date;
-	private double baseSalary;
+	private Double baseSalary;
 	private Department department;
 
 	public Seller() {
 	}
 
-	public Seller(int id, String name, String email, LocalDate date, double baseSalary, Department department) {
+	public Seller(Integer id, String name, String email, LocalDate date, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
