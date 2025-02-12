f9187479b975696145fc19e46fab0677461ab402
JrD <198513937+jrd-dev0@users.noreply.github.com>
2025-02-12 12:18:36 -0300

    Validation Test
    Teste de validação das classes e métodos criados

--- Java/Public/demo-dao-jdbc/src/application/Program.java
+++ Java/Public/demo-dao-jdbc/src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.time.LocalDate;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Department dep1 = new Department(1, "Books");
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, dep1);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println(dep1);
+		System.out.println(seller);
 
 	}
-
 }
