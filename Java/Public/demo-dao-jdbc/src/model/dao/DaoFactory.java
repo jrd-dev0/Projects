dacdda7b00c3a6c2f7beaca1cebc4c421dc07418
JrD <198513937+jrd-dev0@users.noreply.github.com>
2025-02-13 11:20:58 -0300

    Created DepartmentDaoJDBC and added his reference at DaoFactory
    Criado DepartmentDaoJDBC e adicionado sua referencia em DaoFactory

--- Java/Public/demo-dao-jdbc/src/model/dao/DaoFactory.java
+++ Java/Public/demo-dao-jdbc/src/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
 import db.DBConnection;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DBConnection.getConnection()); // Implementando a interface SellerDao com o método de persistência JDBC
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DBConnection.getConnection()); // Implementando a interface DepartmentDao com o método de persistência JDBC
 	}
 }
