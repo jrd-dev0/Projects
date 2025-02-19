67b4ad0d13427884223e0f8a54dc69d0bd0e97b2
JrD <198513937+jrd-dev0@users.noreply.github.com>
2025-02-19 09:18:54 -0300

    Service layer, component registration

---
+++ Java/Public/WebServices-SpringBoot-JPA-Hibernate/src/main/java/edu/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package edu.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.educandoweb.course.entities.User;
+import edu.educandoweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();
+	}
+
+}
