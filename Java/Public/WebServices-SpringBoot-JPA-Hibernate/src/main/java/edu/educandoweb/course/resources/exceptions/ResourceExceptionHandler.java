34218f344b19dac4106a6e065e38298a7a030e51
JrD <198513937+jrd-dev0@users.noreply.github.com>
2025-02-25 19:50:35 -0300

    Exception handling - delete

--- Java/Public/WebServices-SpringBoot-JPA-Hibernate/src/main/java/edu/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ Java/Public/WebServices-SpringBoot-JPA-Hibernate/src/main/java/edu/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import edu.educandoweb.course.services.exceptions.DatabaseException;
 import edu.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -19,6 +20,14 @@ public class ResourceExceptionHandler  {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); 
 		return ResponseEntity.status(status).body(standardError);
-		
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); 
+		return ResponseEntity.status(status).body(standardError);
+	}
+	
 }
